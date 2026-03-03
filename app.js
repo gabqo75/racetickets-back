d26a47f553be4457af0572e3ab94e9d2f8d9d5e0
plambert684 <plambert684@gmail.com>
2026-03-03 20:59:55 +0100

    patch

--- app.js
+++ app.js
@@ -9,10 +9,7 @@ dns.setDefaultResultOrder('ipv4first');
 
 const app = express();
 const db = require('./db');
-db.query(`ALTER TABLE users ADD COLUMN IF NOT EXISTS phone_number VARCHAR(20)`, (err) => {
-    if (err) console.error("Erreur lors de l'ajout de la colonne phone_number:", err);
-    else console.log("Colonne phone_number vérifiée/ajoutée.");
-});
+
 app.use(cors({
   origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
@@ -31,7 +28,7 @@ const eventRoutes = require('./routes');
 app.use('/api', eventRoutes);
 
 io.on('connection', (socket) => {
-    console.log('📱 Un utilisateur s\'est connecté en temps réel');
+    console.log('Un utilisateur s\'est connecté');
 });
 
 const PORT = process.env.PORT || 3001;
